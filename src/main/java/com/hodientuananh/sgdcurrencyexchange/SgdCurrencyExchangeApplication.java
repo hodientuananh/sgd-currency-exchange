1c80a11ef30ade5baa7304f6275c3794433b4ed5
Tuan Anh Ho Dien <benkinmat@gmail.com>
2020-01-08 13:32:12 +0700

    initial project

---
+++ src/main/java/com/hodientuananh/sgdcurrencyexchange/SgdCurrencyExchangeApplication.java
@@ -0,0 +1,13 @@
+package com.hodientuananh.sgdcurrencyexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgdCurrencyExchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgdCurrencyExchangeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hodientuananh/sgdcurrencyexchange/SgdCurrencyExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hodientuananh.sgdcurrencyexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgdCurrencyExchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
